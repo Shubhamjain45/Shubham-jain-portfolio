b4b972b4f2a915ed3bf46aa19b8da3855d1bff37
Shubhamjain45 <60482023+Shubhamjain45@users.noreply.github.com>
2021-07-31 17:54:26 +0530

    Updates

---
+++ precache-manifest.0b5b1ce14c4dd33cb3849a0e01a674dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e67433a6dec383e97b4d2e0206bf42",
+    "url": "/Shubham-jain-portfolio/index.html"
+  },
+  {
+    "revision": "49427acb28fa46ddea1a",
+    "url": "/Shubham-jain-portfolio/static/css/main.3b4a8120.chunk.css"
+  },
+  {
+    "revision": "c6ca6c84ee2a02bfc5f5",
+    "url": "/Shubham-jain-portfolio/static/js/2.75376d99.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Shubham-jain-portfolio/static/js/2.75376d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49427acb28fa46ddea1a",
+    "url": "/Shubham-jain-portfolio/static/js/main.514e01ec.chunk.js"
+  },
+  {
+    "revision": "98014adc6698ba4118d7",
+    "url": "/Shubham-jain-portfolio/static/js/runtime-main.3d546b21.js"
+  },
+  {
+    "revision": "346f5c8f1d04a5a84eed7e78c66a32b4",
+    "url": "/Shubham-jain-portfolio/static/media/resume.346f5c8f.pdf"
+  },
+  {
+    "revision": "0e63bb104e8eff71f96e87d73f3b4392",
+    "url": "/Shubham-jain-portfolio/static/media/shubhamjain.0e63bb10.png"
+  }
+]);
